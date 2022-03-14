ad1d8b6b3f9445bd6fc60128142c6cc108e7d58c
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-03-14 20:24:48 +0100

    Controls functionality

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const controlBtns = document.querySelectorAll('[data-controls]');
 controlBtns.forEach(btn => btn.addEventListener('click', () => {
     const items = btn.closest('[data-category]')
                      .querySelector('[data-items]');
-    console.log(items);
+
+    if (btn.dataset.controls === 'prev') items.style.transform += 'translateX(100%)';
+    if (btn.dataset.controls === 'next') items.style.transform += 'translateX(-100%)';
+    
+
 }));
 
 
