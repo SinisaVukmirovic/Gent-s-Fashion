2d952fa20ffe40aacb24efa9027f0baecdb7574d
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-03-14 21:07:20 +0100

    To DO hide btns on end

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 const controlBtns = document.querySelectorAll('[data-controls]');
+let currentItem = 1;
 
 controlBtns.forEach(btn => btn.addEventListener('click', () => {
     const items = btn.closest('[data-category]')
                      .querySelector('[data-items]');
 
-    if (btn.dataset.controls === 'prev') items.style.transform += 'translateX(100%)';
-    if (btn.dataset.controls === 'next') items.style.transform += 'translateX(-100%)';
+    let numbOfItems = items.querySelectorAll('.item').length;
+    console.log(numbOfItems, currentItem);
+
+    if (btn.dataset.controls === 'next') {
+        currentItem = currentItem + 1;
+        items.style.transform += 'translateX(-100%)';
+    }
+
+    // if (btn.dataset.controls === 'prev') items.style.transform += 'translateX(100%)';
+    // if (btn.dataset.controls === 'next') items.style.transform += 'translateX(-100%)';
     
 
 }));
