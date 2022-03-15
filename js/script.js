47264abdc0d5f28d717ea373f707164915aa0f43
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-03-15 21:59:41 +0100

    Trouble add/remove hide cntr btn

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
 const controlBtns = document.querySelectorAll('[data-controls]');
-let currentItem = 1;
 
 controlBtns.forEach(btn => btn.addEventListener('click', () => {
+    // if (btn.classList.contains('hide')) console.log('sdf')
+    // btn.classList.remove('hide');
+    // console.log(btn.dataset.controls == 'next')
+
+
     const step = btn.dataset.controls === 'next' ? 1 : -1;
 
     const items = btn.closest('[data-category]')
                      .querySelector('[data-items]');
 
-    // let numbOfItems = items.querySelectorAll('.item').length;
-    // console.log(numbOfItems, currentItem);
+    let numbOfItems = items.querySelectorAll('.item').length;
+    console.log(numbOfItems);
 
     let activeSlide = items.querySelector('[data-active]');
-    console.log(activeSlide)
-
+    // console.log(activeSlide)
     let activeIndex = [...items.children].indexOf(activeSlide) + step;
+
+    if (activeIndex === numbOfItems - 1) {
+        btn.classList.add('hide');
+    } 
+    if (activeIndex >= 1) {
+        
+        btn.classList.remove('hide');
+    }   
 
     items.children[activeIndex].dataset.active = 'true';
     delete activeSlide.dataset.active;
 
     console.log(activeIndex)
+
+    // if (activeIndex === numbOfItems) btn.classList.add('hide');
 
 
     if (btn.dataset.controls === 'next') items.style.transform += `translateX(-100%)`;
@@ -35,8 +48,3 @@ controlBtns.forEach(btn => btn.addEventListener('click', () => {
     
 
 }));
-
-
-
-
-console.log(controlBtns);
