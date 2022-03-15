3c69635ad6faa0cf2f5831589e88b0d0a06894c9
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-03-16 00:04:59 +0100

    Basic fFunctionality done

--- js/script.js
+++ js/script.js
@@ -1,50 +1,31 @@
 const controlBtns = document.querySelectorAll('[data-controls]');
 
 controlBtns.forEach(btn => btn.addEventListener('click', () => {
-    // if (btn.classList.contains('hide')) console.log('sdf')
-    // btn.classList.remove('hide');
-    // console.log(btn.dataset.controls == 'next')
-
-
     const step = btn.dataset.controls === 'next' ? 1 : -1;
 
     const items = btn.closest('[data-category]')
                      .querySelector('[data-items]');
 
     let numbOfItems = items.querySelectorAll('.item').length;
-    console.log(numbOfItems);
-
+    
     let activeSlide = items.querySelector('[data-active]');
-    // console.log(activeSlide)
+    
     let activeIndex = [...items.children].indexOf(activeSlide) + step;
-
-    if (activeIndex === numbOfItems - 1) {
-        btn.classList.add('hide');
-    } 
-    if (activeIndex >= 1) {
-        
-        btn.classList.remove('hide');
-    }   
 
     items.children[activeIndex].dataset.active = 'true';
     delete activeSlide.dataset.active;
 
-    console.log(activeIndex)
+    if (btn.dataset.controls === 'next') {
+        btn.parentElement.querySelector('.pointer-left').classList.remove('hide');
+        
+        if (activeIndex == numbOfItems - 1) btn.parentElement.querySelector('.pointer-right').classList.add('hide'); 
 
-    // if (activeIndex === numbOfItems) btn.classList.add('hide');
-
-
-    if (btn.dataset.controls === 'next') items.style.transform += `translateX(-100%)`;
-    if (btn.dataset.controls === 'prev') items.style.transform += `translateX(100%)`;
-
-
-    // if (btn.dataset.controls === 'next') {
-    //     currentItem = currentItem + 1;
-    //     items.style.transform += 'translateX(-100%)';
-    // }
-
-    // if (btn.dataset.controls === 'prev') items.style.transform += 'translateX(100%)';
-    // if (btn.dataset.controls === 'next') items.style.transform += 'translateX(-100%)';
+        items.style.transform += 'translateX(-100%)';
+    }
     
+    if (btn.dataset.controls === 'prev') {
+        if (activeIndex == 0) btn.parentElement.querySelector('.pointer-left').classList.add('hide'); 
 
+        items.style.transform += 'translateX(100%)';  
+    }     
 }));
